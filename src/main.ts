19c7d94c70004f7880f7ace86cca9899d5a21bfa
Danny <danny.hj.vogel@gmail.com>
2025-06-01 21:58:22 +0200

    Chore: bump version to 0.1.3; update payload structure to use device_id for push notification subscription

--- src/main.ts
+++ src/main.ts
@@ -86,7 +86,7 @@ export function usePushNotifications(
       const response = await fetch(subscribeEndpoint, {
         method: "POST",
         body: JSON.stringify({
-          deviceId,
+          device_id: deviceId,
           subscription,
         }),
         headers: {
